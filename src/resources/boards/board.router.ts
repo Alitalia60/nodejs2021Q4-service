161bc5747c61e19dbf38d6ddfd6ce3e25632cc22
Alitalia60 <lita.study@bk.ru>
2021-12-23 17:54:24 +0300

    refactor: add logger module on pino

--- src/resources/boards/board.router.ts
+++ src/resources/boards/board.router.ts
@@ -4,24 +4,28 @@ import Router from "koa-router";
 import { getBoard, addBoard, updBoard, delBoard } from "./board.service";
 import { getBoardList } from "./board.memory.repository";
 import { koaContext } from "../../common/types";
+
+import logger from '../../common/logger'
+
 export const boardRouter = new Router();
 
-boardRouter.get("/boards", async (ctx:koaContext) => {
+boardRouter.get("/boards", async (ctx: koaContext) => {
+  logger.info('GET logger')
   getBoardList(ctx);
 });
 
-boardRouter.get("/boards/:boardId", koaBody(), async (ctx:koaContext) => {
+boardRouter.get("/boards/:boardId", koaBody(), async (ctx: koaContext) => {
   getBoard(ctx);
 });
 
-boardRouter.post("/boards", koaBody(), async (ctx:koaContext) => {
+boardRouter.post("/boards", koaBody(), async (ctx: koaContext) => {
   addBoard(ctx);
 });
 
-boardRouter.put("/boards/:boardId", koaBody(), async (ctx:koaContext) => {
+boardRouter.put("/boards/:boardId", koaBody(), async (ctx: koaContext) => {
   updBoard(ctx);
 });
 
-boardRouter.del("/boards/:boardId", async (ctx:koaContext) => {
+boardRouter.del("/boards/:boardId", async (ctx: koaContext) => {
   delBoard(ctx);
 });
