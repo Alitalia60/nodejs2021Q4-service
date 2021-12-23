2e99905b1f9f1a327bcf9de04881c06f644d5c65
Alitalia60 <lita.study@bk.ru>
2021-12-23 17:28:40 +0300

    fix: fix incorrect tsconfig.json eslintrc.json

--- src/resources/boards/board.router.ts
+++ src/resources/boards/board.router.ts
@@ -3,24 +3,25 @@ import Router from "koa-router";
 
 import { getBoard, addBoard, updBoard, delBoard } from "./board.service";
 import { getBoardList } from "./board.memory.repository";
+import { koaContext } from "../../common/types";
 export const boardRouter = new Router();
 
-boardRouter.get("/boards", async (ctx) => {
+boardRouter.get("/boards", async (ctx:koaContext) => {
   getBoardList(ctx);
 });
 
-boardRouter.get("/boards/:boardId", koaBody(), async (ctx) => {
+boardRouter.get("/boards/:boardId", koaBody(), async (ctx:koaContext) => {
   getBoard(ctx);
 });
 
-boardRouter.post("/boards", koaBody(), async (ctx) => {
+boardRouter.post("/boards", koaBody(), async (ctx:koaContext) => {
   addBoard(ctx);
 });
 
-boardRouter.put("/boards/:boardId", koaBody(), async (ctx) => {
+boardRouter.put("/boards/:boardId", koaBody(), async (ctx:koaContext) => {
   updBoard(ctx);
 });
 
-boardRouter.del("/boards/:boardId", async (ctx) => {
+boardRouter.del("/boards/:boardId", async (ctx:koaContext) => {
   delBoard(ctx);
 });
